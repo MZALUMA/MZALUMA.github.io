7f33049ab231d5c8ed77bb48d1805c5ec7000658
MZALUMA <mzaluma2012@gmail.com>
2021-05-12 20:01:40 +0300

    man izdevas

---
+++ test.js
@@ -0,0 +1,4 @@
+function myFunction() {
+ 
+document.getElementById("demo").innerHTML = "Es gribēju pāriet uz veselīgu uzturu, bet, mizojot mandarīnu, aci iešļakstījās sula…. Sapratu, ka desa nekad man neko tādu nenodarītu"; document.getElementById("poga").innerHTML = "...";
+}
